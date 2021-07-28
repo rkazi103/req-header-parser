9a0c55cde96b5c380f9a9da9a50e8446a19eadb1
Rayan Kazi <rayan.quack.you@gmail.com>
2021-07-28 12:06:03 -0700

    added whoami route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
+router.get("/", (req, res) => {
   res.json({ greeting: "hello API" });
+});
+
+router.get("/whoami", (req, res) => {
+  res.status(200).json({
+    ipaddress: req.header("x-forwarded-for") || req.socket.remoteAddress,
+    language: req.header("accept-language"),
+    software: req.header("user-agent"),
+  });
 });
 
 module.exports = router;
